21cf61c7a71883575d6526d7f6b4ffad1d70e8e8
User <User@SIMPLEBUDDY>
2023-01-31 09:48:14 +0530

    Read commit

---
+++ src/test/java/vTiger/Practice/ReadDataFrom.java
@@ -0,0 +1,28 @@
+package vTiger.Practice;
+
+import org.testng.annotations.Test;
+
+public class ReadDataFrom {
+	@Test
+	public void readData()
+	{
+		String BROWSER = System.getProperty("Browser"); // it will accept run time parameters
+		System.out.println(BROWSER);
+		
+		String URL = System.getProperty("url");
+		System.out.println(URL);
+		
+		String USERNAME = System.getProperty("username");
+		System.out.println(USERNAME);
+		
+		String PASSWORD = System.getProperty("password");
+		System.out.println(PASSWORD);
+		
+		System.out.println("Eexecuted");
+		
+		
+		
+		
+	}
+
+}
